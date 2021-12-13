b56104222e48d6d7627082d22a8f1069e0ad30ec
Jestim <jesper.os.thiman@gmail.com>
2021-12-13 15:41:15 +0100

    Styling and refactoring done

--- main.js
+++ main.js
@@ -28,10 +28,11 @@ async function getWeatherData(location) {
 
 function processWeatherData(weatherDataJson) {
     return {
-        weather: weatherDataJson.weather[0].main,
+        weather: weatherDataJson.weather[0].description,
         temperature: weatherDataJson.main.temp,
         wind: weatherDataJson.wind.speed,
-        feelsLike: weatherDataJson.main.feels_like
+        feelsLike: weatherDataJson.main.feels_like,
+        gifSearchWord: weatherDataJson.weather[0].main
     };
 }
 
@@ -41,18 +42,36 @@ function createWeatherCard(location, weatherDataObj) {
     }
 
     const divEl = document.createElement('div');
-    const paraEl = document.createElement('p');
-    paraEl.textContent = location[0].toUpperCase() + location.slice(1).toLowerCase();
-    divEl.appendChild(paraEl);
+    divEl.classList.add('weather-card')
 
-    for (const [key, value] of Object.entries(weatherDataObj)) {
-        const paraEl = document.createElement('p');
-        paraEl.textContent = `${key}: ${value}`;
-        divEl.appendChild(paraEl);
-    }
+    const cityNameEl = document.createElement('p');
+    cityNameEl.classList.add('city');
+    cityNameEl.textContent = location[0].toUpperCase() + location.slice(1).toLowerCase();
+    divEl.appendChild(cityNameEl);
+
+    appendWeatherData(weatherDataObj, divEl);
 
     mainEl.appendChild(divEl);
-    addGifEl(weatherDataObj.weather);
+    console.log(weatherDataObj.gifSearchWord)
+    addGifEl(weatherDataObj.gifSearchWord);
+}
+
+function appendWeatherData(weatherDataObj, divEl) {
+    const weatherEl = document.createElement('p');
+    weatherEl.textContent = `Weather: ${weatherDataObj.weather[0].toUpperCase() + weatherDataObj.weather.slice(1)}`;
+    divEl.appendChild(weatherEl);
+
+    const temperatureEl = document.createElement('p');
+    temperatureEl.textContent = `Temperature: ${Math.round(weatherDataObj.temperature)} °C`;
+    divEl.appendChild(temperatureEl);
+
+    const windEl = document.createElement('p');
+    windEl.textContent = `Wind: ${weatherDataObj.wind} m/s`;
+    divEl.appendChild(windEl);
+
+    const feelsLikeEl = document.createElement('p');
+    feelsLikeEl.textContent = `Feels like: ${Math.round(weatherDataObj.feelsLike)} °C`;
+    divEl.appendChild(feelsLikeEl);
 }
 
 async function addGifEl(weather) {
